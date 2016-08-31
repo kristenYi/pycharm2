92123278cb9c22702d0b5f5af8e8e1ae9f4af5bf
alfred <mrhnddq@163.com>
2016-08-31 11:46:25 +0800

    Initial commit

---
+++ hhh.py
@@ -0,0 +1,175 @@
+#coding:utf-8
+'''a = 'China'
+a_len = len(a)
+print str(a_len)'''
+
+'''input_string = raw_input()
+a, b, c = (int(x) for x in input_string.split(' '))
+print a+b+c'''
+
+
+'''a = 'hello'
+b = a[0] + 'i'
+print b
+print len(b)'''
+
+'''a = 'love'
+b = a[0] + 'i'
+print b
+print len (b)'''
+
+'''a = 'love'
+a_len = len(a)
+print str(a_len)'''
+
+'''print 155*155'''
+'''print 38 * 38'''
+
+
+'''def add_function(a,b):
+    c = a+b
+    print c
+
+if __name__=="__main__":
+    add_function(2,3)'''
+
+print "hello world"
+'''a = "hello"
+b = len (a)
+print b'''
+
+'''a = "love"
+b = a.upper()
+print b
+c = a.capitalize()
+print c
+d = a.isupper()
+print d
+f = a.islower()
+print f
+print a'''
+
+'''a = "This is a book"
+print a
+b = a.title()
+print b
+c = b.istitle()
+print c'''
+
+'''a = "hello world"
+print len (a)
+print a[-5]
+print a[1:3]
+print a[:]
+print a[2:]
+print a[:3]'''
+
+'''a = " hello "
+print a
+print a.strip()
+print a.lstrip()
+print a.rstrip()'''
+
+'''#coding:utf-8
+print "what is your name ?"
+name = raw_input()
+b = name
+c = b.upper()
+print "hello ,%s"% c'''
+
+'''var =raw_input("please input a var :")
+print var
+print type(var)'''
+
+'''print 20%2
+print 20%12
+print 100//23'''
+
+'''print "hello world"
+temp = raw_input("没有伞的孩子只能努力奔跑 猜猜我最喜欢的人")
+#input() and raw_input()
+#guess = int(temp)
+print temp
+if temp == "cc":
+        print("你很聪明 给你个啵啵")
+        print("时间很快 你要快乐")
+else:
+    print("猜错了 也没事 时间很和蔼")
+    print("time goes by")'''
+'''def sum (n):
+    a = 1
+    sum = 0
+    for a in range(1 , n+1):
+        a = a +1
+        sum = sum + a
+    return sum
+print sum(100)'''
+
+
+'''#coding:utf8
+print('用双手迎接未来')
+n = raw_input(猜猜我的大学专业)
+print n
+if n == "计算机":
+        print ('短短四年 祝你成功')
+else:
+        print ('在努力一把')'''
+'''input_string = raw_input()
+print input_string.split(' ')
+sum = 0
+for x in input_string.split(' '):
+    sum = sum + int(x)
+
+print sum
+a, b, c = (int(x) for x in input_string.split(' '))
+print a+b+c
+'''
+
+
+
+
+'''a = "bab" + "hh"
+print a
+'''
+'''print 2 * 3'''
+# coding:utf8
+'''print ("用双手迎接未来")
+n = raw_input("猜猜我的大学专业")
+print n
+
+if n == "计算机":
+        print ('短短四年 祝你成功')
+else:
+        print ('在努力一把')'''
+
+
+'''a = 'China'
+a_len = len(a)
+b = 'length of a is'
+print b + str(a_len)'''
+
+
+'''print("we are family")
+temp = raw_input("please answer this question , do you like mary")
+print temp
+if temp == "yes":
+    print ("she likes you, too")
+else:
+    print("sorry , she does not like you")'''
+
+
+
+a = "老齐"
+b = "吃 apple"
+c = a + b
+print c
+
+'''a = input("please input a :")
+b = input("please input b :")
+print a
+print b
+if  a > b:
+    print "哈哈哈"
+else:
+    print "嘻嘻嘻"
+'''
